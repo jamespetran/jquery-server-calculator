782795db5d3ac97613523f0d923faadd6ff2f018
James Petran <jm.ptrn@gmail.com>
2021-12-17 21:23:20 -0600

    clear functionality & history div

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,11 @@ $(onReady);
 console.log('js');
 
 let operator = '';
+let submission = {
+      firstNum: '',
+      operator: '',
+      secondNum: '',
+}
 
 function onReady() {
       console.log('jq');
@@ -15,11 +20,26 @@ function operatorSelect() {
       operator = $(this).text(); //set operator to the text of the button, +,-,X,÷
       $('#calculator button').removeClass("selected"); //blank out all blacked out buttons
       $(this).addClass("selected"); // set button to black to show it is selected
-      console.log(operator); // log the selected operator to check that its right
+      // console.log(operator); // log the selected operator to check that its right
+      if (operator === "+") {
+            plus();
+      } else if (operator === "-") {
+            minus();
+      } else if (operator === "X") {
+            multiply();
+      } else if (operator === "÷") {
+            divide();
+      } else {console.log("something went wrong");}
+      return;
+
 } // end operatorSelect
 
 function onSubmit() {
       console.log('submitting');
+      submission.firstNum = $('#firstNumber').val();
+      submission.secondNum = $('#secondNumber').val();
+
+      
       //code that submits the inputs & operator to server
       //POST
 
@@ -30,5 +50,36 @@ function onSubmit() {
 function onClear() {
       console.log('clearing');
       //code that clears the inputs, operator, and history
+      $('#calculator button').removeClass("selected"); //blank out all blacked out buttons
+      $('#firstNumber').val('');
+      $('#secondNumber').val('');
+      operator = '';
+      submission.firstNum='';
+      submission.secondNum='';
+      submission.operator=''; // zeroing out the storage inputs & submission object
+      $('#history').text(''); // blanking out the historical display of equations
+
+
       //POST
+}
+
+function plus() {
+      let operator="plus";
+      console.log(operator);
+      submission.operator = operator;
+}
+function minus() {
+      let operator="minus";
+      console.log(operator);
+      submission.operator = operator;
+}
+function multiply() {
+      let operator="multiply";
+      console.log(operator);
+      submission.operator = operator;
+}
+function divide() {
+      let operator="divide";
+      console.log(operator);
+      submission.operator = operator;
 }
